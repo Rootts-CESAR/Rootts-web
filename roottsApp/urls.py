9779494405a80f6376495d9901205b2c4974059e
Davi Aleixo <dac2@cesar.school>
2022-09-30 19:53:02 -0300

    RTTS-20: Added login functionality

--- roottsApp/urls.py
+++ roottsApp/urls.py
@@ -1,9 +1,13 @@
-from django.urls import path
+from django.contrib import admin
+from django.contrib.auth import views as auth_views 
+from django.urls import path, include
 
 from .views import IndexView, EncostaView, CreateEncostaView, UpdateEncostaView, DeleteEncostaView,DenunciaFormView
 
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', IndexView, name='index'),
     path('crud/', EncostaView, name='crud'),
     path('create/', CreateEncostaView, name='add_encosta'),
