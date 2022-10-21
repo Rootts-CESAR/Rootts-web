1cd7b7ef8a0307d6626285b6b2161984b15ec20a
hendrich <arthuralenc@gmail.com>
2022-10-21 18:12:28 -0300

    RTTS-59: Visualizar Risco

--- roottsApp/models.py
+++ roottsApp/models.py
@@ -6,13 +6,22 @@ from django.contrib.auth.models import AbstractUser
 class Encosta(models.Model):
     nome = models.CharField(max_length=15)
     local = models.CharField(max_length=40)
-    latitude = models.DecimalField(max_digits=15, decimal_places=10)
-    longitude = models.DecimalField(max_digits=15, decimal_places=10)
-    declividade = models.DecimalField(max_digits=15, decimal_places=10)
-    numeroConstrucoes = models.DecimalField(max_digits=15, decimal_places=10)
-    coeficienteUmidade = models.DecimalField(max_digits=15, decimal_places=10)
-    proximidadeRedeViarias = models.DecimalField(max_digits=15, decimal_places=10)
-    proximidadeCorposLiquidos = models.DecimalField(max_digits=15, decimal_places=10)
+    latitude = models.DecimalField(max_digits=20, decimal_places=10)
+    longitude = models.DecimalField(max_digits=20, decimal_places=10)
+    declividade = models.DecimalField(max_digits=20, decimal_places=10)
+    numeroConstrucoes = models.DecimalField(max_digits=20, decimal_places=10)
+    coeficienteUmidade = models.DecimalField(max_digits=20, decimal_places=10)
+    proximidadeRedeViarias = models.DecimalField(max_digits=20, decimal_places=10)
+    proximidadeCorposLiquidos = models.DecimalField(max_digits=20, decimal_places=10)
+
+    PRIORITY_LEVELS = (
+        ('Muito Baixo', 'Muito Baixo'),
+        ('Baixo', 'Baixo'),
+        ('Médio', 'Médio'),
+        ('Alto', 'Alto'),
+        ('Crítico', 'Crítico'),
+    )
+    prioridadeEncosta = models.CharField(max_length=30, choices=PRIORITY_LEVELS, default = 'Muito Baixo')
 
     def __str__(self):
         return self.nome, self.local
